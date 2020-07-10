1756ba928cdbd0c829e08fa6a26807a90746cbe0
Sang Truong <sangtruong_2021@depauw.edu>
2020-07-10 10:41:54 -0400

    Update ISRU.py

--- ISRU.py
+++ ISRU.py
@@ -1,3 +1,4 @@
+# **ISRU Function**
 import math
 def ISRU(x):
   a = 1
@@ -16,3 +17,28 @@ plt.plot(x, y)
 plt.show()
 
 ISRU(0.23)
+
+# **Convert Relative Likelihood to Probability using ISRU**
+import pandas as pd
+import math
+from google.colab import drive
+drive.mount('/content/gdrive/')
+
+path = 'gdrive/My Drive/Colab Notebooks/GR-TrainModel/Alucard/'
+df = pd.read_csv(path+'2M_Simple_All_OHE_noDup.csv')
+
+# Define Inverse Square Root Unit function
+def ISRU(x):
+  a = 1
+  if x == 1 or x == 0:
+    return x
+  else:
+    return x / (math.sqrt(a + x**2))
+t = df.columns.tolist()
+
+# Iterate through each e-column
+for i in range(57, 109):
+  df[t[i]] = df[t[i]].apply(ISRU)
+
+# Save to csv
+df.to_csv(path+'2M_Simple_All_OHE_noDup_ISRU.csv', index=False)
